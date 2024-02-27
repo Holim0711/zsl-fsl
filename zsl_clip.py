e91fb7003903448e3587eedc9ea88f998982d889
Holim Lim <holim0711@outlook.com>
2024-02-27 18:44:45 +0900

    zsl

--- zsl_clip.py
+++ zsl_clip.py
@@ -2,7 +2,8 @@ import os
 import sys
 import torch
 from tqdm import tqdm
-from utils import zeroshot_classifier, load_clip, load_test_dataset
+from utils import zeroshot_classifier, load_clip
+from datasets import build_coop_datasets
 import yaml
 
 
@@ -33,19 +34,17 @@ def test_inference(loader, model, zeroshot_weights):
 if __name__ == "__main__":
     model_name = sys.argv[1] if len(sys.argv) > 1 else 'RN50'
     dataset_name = sys.argv[2] if len(sys.argv) > 2 else 'imagenet'
-    sfs = (sys.argv[3] == 'sfs') if len(sys.argv) > 3 else None
+    dataset_root = os.environ['TORCHVISION_DATASETS']
 
     # load model & dataset
     model, preprocess = load_clip(model_name)
-    dataset = load_test_dataset(dataset_name, preprocess)
-    loader = torch.utils.data.DataLoader(dataset, batch_size=32, num_workers=os.cpu_count())
+    datasets = build_coop_datasets(dataset_name, dataset_root, preprocess, preprocess)
+    loader = torch.utils.data.DataLoader(datasets['test'], batch_size=32, num_workers=os.cpu_count())
 
     # load classes & templates
-    class_path = os.path.join('data', 'CLIP', dataset_name)
+    class_path = os.path.join('data', 'CoOp', dataset_name)
     classes = yaml.safe_load(open(os.path.join(class_path, 'classes.yaml')))
     templates = yaml.safe_load(open(os.path.join(class_path, 'templates.yaml')))
-    if sfs and dataset_name == 'imagenet':
-        templates = yaml.safe_load(open(os.path.join(class_path, 'templates.sfs.yaml')))
     print(f"{len(classes)} classes, {len(templates)} templates")
 
     # build classifier
