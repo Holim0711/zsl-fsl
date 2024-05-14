b009139c75f392dd0d89ba7eb40348382ed6791d
Holim Lim <holim0711@outlook.com>
2024-05-14 18:34:44 +0900

    remove face-easy

--- src/zsfs/datasets/__init__.py
+++ src/zsfs/datasets/__init__.py
@@ -1,16 +1,6 @@
 from .torchvision import get_datasets as get_torchvision_datasets
-from .coop.coop import get_datasets as get_coop_datasets
+from .coop import get_datasets as get_coop_datasets
 
 
-def get_datasets(
-    module: str,
-    name: str,
-    train_transform=None,
-    test_transform=None,
-):
-    if module.lower() == 'torchvision':
-        return get_torchvision_datasets(name, train_transform, test_transform)
-    elif module.lower() == 'coop':
-        return get_coop_datasets(name, train_transform, test_transform)
-    else:
-        raise ValueError(f'Unknown module: {module}')
+def get_datasets(name, train_transform=None, test_transform=None):
+    return get_coop_datasets(name, train_transform, test_transform)
