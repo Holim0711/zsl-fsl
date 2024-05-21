c523c95266c7a2dfb77bc9f9c0c90f01befa06b8
Holim Lim <holim0711@outlook.com>
2024-05-21 14:48:07 +0900

    wow

--- flyp_fewshot.py
+++ flyp_fewshot.py
@@ -14,8 +14,13 @@ import torch.nn as nn
 
 
 @torch.no_grad()
-def run_test(loader, model, zeroshot_classifier):
-    model.eval()
+def run_test(
+    model: CLIP,
+    loader: DataLoader,
+    prompts: list[list[str]],
+):
+    zeroshot_classifier = MeanEnsembler(encode_prompts(model, prompts))
+    zeroshot_classifier.eval()
     n_right, n_total = 0, 0
     for x, y in tqdm(loader):
         x, y = x.cuda(), y.cuda()
@@ -28,17 +33,37 @@ def run_test(loader, model, zeroshot_classifier):
 
 def run_train(
     model: CLIP,
-    train_loader: DataLoader,
-    val_loader: DataLoader,
+    train_dataset: Dataset,
+    val_dataset: Dataset,
+    prompts: list[list[str]],
     total_epochs: int = 1,
     warmup_epochs: float = 0,
     lr: float = 1e-5,
-    weight_decay: float = 0.0,
+    wd: float = 0.0,
+    eps: float = 1e-8,
+    batch_size: int = 1,
 ):
-    n_iters = len(train_loader)
-    total_iters = total_epochs * n_iters
-    warmup_iters = int(warmup_epochs * n_iters)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    save_path = f'best_model_{total_epochs}_{warmup_epochs}_{lr}_{wd}_{eps}_{batch_size}.pth'
+
+    train_loader = DataLoader(
+        FLYPDataset(train_dataset, prompts),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+        drop_last=True,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        num_workers=os.cpu_count(),
+        pin_memory=True
+    )
+
+    num_iters = len(train_loader)
+    total_iters = total_epochs * num_iters
+    warmup_iters = int(warmup_epochs * num_iters)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=wd, eps=eps)
     if warmup_iters > 0:
         scheduler1 = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=(1 / warmup_iters), total_iters=warmup_iters)
         scheduler2 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=(total_iters - warmup_iters))
@@ -47,9 +72,13 @@ def run_train(
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=total_iters)
     print('start training')
 
-    for curr_epoch in range(total_epochs):
+    best_acc = 0.0
+    best_epoch = 0
+
+    for curr_epoch in range(1, total_epochs + 1):
         model.train()
-        for x, y in tqdm(train_loader):
+        pbar_info = f'[acc] {best_acc}@{best_epoch}, [epoch] {curr_epoch}/{total_epochs}'
+        for x, y in tqdm(train_loader, desc=pbar_info):
             x, y = x.cuda(), y.cuda()
             logits_per_image, logits_per_text = model(x, y)
             labels = torch.arange(x.size(0)).cuda()
@@ -61,11 +90,13 @@ def run_train(
             scheduler.step()
 
         model.eval()
-        zeroshot_classifier = MeanEnsembler(encode_prompts(model, prompts))
-        acc = run_test(val_loader, model, zeroshot_classifier)
-        print('val acc:', acc)
-        break
+        acc = run_test(model, val_loader, prompts)
+        if acc > best_acc:
+            best_acc = acc
+            best_epoch = curr_epoch
+            torch.save(model.state_dict(), save_path)
 
+    model.load_state_dict(torch.load(save_path))
     model.eval()
     return model
 
@@ -93,7 +124,8 @@ if __name__ == "__main__":
     total_epochs = int(sys.argv[4]) if len(sys.argv) > 4 else 1
     warmup_epochs = float(sys.argv[5]) if len(sys.argv) > 5 else 0.0
     lr = float(sys.argv[6]) if len(sys.argv) > 6 else 1e-5
-    weight_decay = float(sys.argv[7]) if len(sys.argv) > 7 else 0.0
+    wd = float(sys.argv[7]) if len(sys.argv) > 7 else 0.0
+    random_seed = int(sys.argv[8]) if len(sys.argv) > 8 else None
     batch_size = 64
 
     # load model & transforms
@@ -105,19 +137,26 @@ if __name__ == "__main__":
     # load datasets
     datasets = get_datasets(dataset_name, train_preprocess, val_preprocess)
     datasets = {
-        'train' : FewShotSubset(datasets['train'], 16, random_seed=7),
-        'val' : FewShotSubset(datasets['val'], 16, random_seed=7, oversampling=True),
+        'train' : FewShotSubset(datasets['train'], 16, random_seed=random_seed),
+        'val' : FewShotSubset(datasets['val'], 16, random_seed=random_seed, oversampling=True),
         'test' : datasets['test']
     }
-    train_dataset = FLYPDataset(datasets['train'], prompts)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=os.cpu_count(), pin_memory=True, drop_last=True)
-    val_loader = DataLoader(datasets['val'], batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
     test_loader = DataLoader(datasets['test'], batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
 
     # train
-    model = run_train(model, train_loader, val_loader, total_epochs=total_epochs)
+    model = run_train(
+        model,
+        datasets['train'],
+        datasets['val'],
+        prompts,
+        total_epochs=total_epochs,
+        warmup_epochs=warmup_epochs,
+        lr=lr,
+        wd=wd,
+        batch_size=batch_size
+    )
 
     # run test
     zeroshot_classifier = MeanEnsembler(encode_prompts(model, prompts))
     acc = run_test(test_loader, model, zeroshot_classifier)
-    print(method_name, model_name, dataset_name, total_epochs, warmup_epochs, lr, weight_decay, batch_size, acc, sep='\t', flush=True)
+    print(method_name, model_name, dataset_name, total_epochs, warmup_epochs, lr, wd, batch_size, acc, sep='\t', flush=True)
